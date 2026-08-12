b36743e4b2e238d284a85b111cf85b5d37f43d2e
josephsmithberger <josephsmithberger@gmail.com>
2026-08-12 12:55:58 -0400

    dialogue balloon

--- scripts/game_control.gd
+++ scripts/game_control.gd
@@ -2,6 +2,13 @@ extends Node
 ## Global source of truth for whether the 3D game viewport is currently controllable.
 
 signal controllability_changed(is_controllable: bool)
+signal input_mode_changed(input_mode: InputMode)
+signal dialogue_activity_changed(is_active: bool)
+
+enum InputMode {
+	KEYBOARD,
+	TOUCH,
+}
 
 var is_controllable := false:
 	set(value):
@@ -10,6 +17,68 @@ var is_controllable := false:
 		is_controllable = value
 		controllability_changed.emit(is_controllable)
 
+var input_mode: InputMode = InputMode.KEYBOARD:
+	set(value):
+		if input_mode == value:
+			return
+		input_mode = value
+		input_mode_changed.emit(input_mode)
+
+var _controllability_requested := false
+var _active_dialogues := 0
+
+
+func _ready() -> void:
+	input_mode = InputMode.TOUCH if DisplayServer.is_touchscreen_available() else InputMode.KEYBOARD
+	_connect_dialogue_manager.call_deferred()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch or event is InputEventScreenDrag:
+		# Mouse-to-touch emulation is useful for testing joysticks, but it should not
+		# make a desktop web player look like a touchscreen device.
+		if event.device != InputEvent.DEVICE_ID_EMULATION:
+			input_mode = InputMode.TOUCH
+	elif event is InputEventKey and event.pressed and not event.echo:
+		input_mode = InputMode.KEYBOARD
+
 
 func set_controllable(value: bool) -> void:
-	is_controllable = value
+	_controllability_requested = value
+	_refresh_controllability()
+
+
+func is_using_touch() -> bool:
+	return input_mode == InputMode.TOUCH
+
+
+func is_dialogue_active() -> bool:
+	return _active_dialogues > 0
+
+
+func _connect_dialogue_manager() -> void:
+	var dialogue_manager := get_node_or_null("/root/DialogueManager")
+	if dialogue_manager == null:
+		push_error("DialogueManager autoload is missing")
+		return
+
+	dialogue_manager.connect(&"dialogue_started", _on_dialogue_started)
+	dialogue_manager.connect(&"dialogue_ended", _on_dialogue_ended)
+
+
+func _on_dialogue_started(_resource: Resource) -> void:
+	_active_dialogues += 1
+	if _active_dialogues == 1:
+		dialogue_activity_changed.emit(true)
+	_refresh_controllability()
+
+
+func _on_dialogue_ended(_resource: Resource) -> void:
+	_active_dialogues = maxi(_active_dialogues - 1, 0)
+	if _active_dialogues == 0:
+		dialogue_activity_changed.emit(false)
+	_refresh_controllability()
+
+
+func _refresh_controllability() -> void:
+	is_controllable = _controllability_requested and not is_dialogue_active()
--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -3,22 +3,58 @@ extends Control
 const KITCHEN_TAB := 0
 
 @onready var _tabs: TabContainer = $MarginContainer/HSplitContainer/TabContainer
+@onready var _dialogue_slot: SubViewportContainer = $MarginContainer/HSplitContainer/TabContainer/Kitchen/PanelContainer/VBoxContainer/DialogueSlot
+@onready var _dialogue_viewport: SubViewport = $MarginContainer/HSplitContainer/TabContainer/Kitchen/PanelContainer/VBoxContainer/DialogueSlot/DialogueViewport
 @onready var _control_deck: Control = $MarginContainer/HSplitContainer/TabContainer/Kitchen/PanelContainer/VBoxContainer/ControlDeck
+@onready var _joystick_row: Control = $MarginContainer/HSplitContainer/TabContainer/Kitchen/PanelContainer/VBoxContainer/ControlDeck/DeckMargin/JoystickRow
+@onready var _keyboard_hint: Control = $MarginContainer/HSplitContainer/TabContainer/Kitchen/PanelContainer/VBoxContainer/ControlDeck/KeyboardHint
+
+var _previous_dialogue_host_resolver: Callable
 
 
 func _ready() -> void:
 	_tabs.tab_changed.connect(_on_tab_changed)
 	GameControl.controllability_changed.connect(_on_controllability_changed)
+	GameControl.input_mode_changed.connect(_on_input_mode_changed)
+	GameControl.dialogue_activity_changed.connect(_on_dialogue_activity_changed)
+
+	_previous_dialogue_host_resolver = DialogueManager.get_current_scene
+	DialogueManager.get_current_scene = _get_dialogue_host
+
+	_on_input_mode_changed(GameControl.input_mode)
 	_on_tab_changed(_tabs.current_tab)
 
 
 func _exit_tree() -> void:
+	DialogueManager.get_current_scene = _previous_dialogue_host_resolver
 	GameControl.set_controllable(false)
 
 
 func _on_tab_changed(tab_index: int) -> void:
 	GameControl.set_controllable(tab_index == KITCHEN_TAB)
+	_update_interaction_ui()
 
 
-func _on_controllability_changed(is_controllable: bool) -> void:
-	_control_deck.visible = is_controllable
+func _on_controllability_changed(_is_controllable: bool) -> void:
+	_update_interaction_ui()
+
+
+func _on_dialogue_activity_changed(_is_active: bool) -> void:
+	_update_interaction_ui()
+
+
+func _update_interaction_ui() -> void:
+	var kitchen_is_active := _tabs.current_tab == KITCHEN_TAB
+	var dialogue_is_active := GameControl.is_dialogue_active()
+	_dialogue_slot.visible = kitchen_is_active and dialogue_is_active
+	_control_deck.visible = kitchen_is_active and GameControl.is_controllable and not dialogue_is_active
+
+
+func _get_dialogue_host() -> Node:
+	return _dialogue_viewport
+
+
+func _on_input_mode_changed(_input_mode: GameControl.InputMode) -> void:
+	var using_touch := GameControl.is_using_touch()
+	_joystick_row.visible = using_touch
+	_keyboard_hint.visible = not using_touch
